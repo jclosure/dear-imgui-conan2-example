922dcb9f4ad861fc303233805632d00feff7d03b
Joel Holder <jholder@magicleap.com>
2024-11-20 17:07:28 -0600

    add crtp optimization example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "main_triangle.cpp"
 #include "main_app.cpp"
 #include "main_demo.cpp"
+#include "main_app_crtp.cpp"
+
 
 int main(int, char**) {
 
@@ -8,7 +10,8 @@ int main(int, char**) {
     
     // main_demo(0, nullptr);
     // main_triangle(0, nullptr);
-    main_app(0, nullptr);
+    // main_app(0, nullptr);
+    main_app_crtp(0, nullptr);
 
     return 0;
 }
---
+++ main_app_crtp.cpp
@@ -0,0 +1,74 @@
+#include "app_design_crtp.hpp"
+class MyAppCRTP : public AppDesignCRTP<MyAppCRTP>
+{
+public:
+    MyAppCRTP() {};
+    ~MyAppCRTP() {};
+
+    void Startup()
+    {
+        printf("Starting up");
+    }
+    void Update()
+    {
+        // Setup Dear ImGui context
+        auto io = ImGui::GetIO();
+        // (void)io;
+
+        // TODO(jh): move these to ctor
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, CRTP!!!"); // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            // TODO(jh): speak w/ ahovington about this
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+    }
+
+private:
+    bool show_demo_window = true;
+    bool show_another_window = false;
+};
+
+int main_app_crtp(int, char **)
+{
+    // works as templated version of MyApp class
+    MyAppCRTP myAppCRTP;
+    myAppCRTP.Run();
+
+    return 0;
+}

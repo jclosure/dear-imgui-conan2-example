03655a571bad6435f399dc09e30acff15dcfeb64
Joel Holder <jholder@magicleap.com>
2024-11-21 02:17:17 -0600

    add click and drag to control translation

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main(int, char**) {
     // Instructions: comment in/out the entry points below to swap apps
     
     // main_demo(0, nullptr);
-    // main_triangle(0, nullptr);
+    main_triangle(0, nullptr);
     // main_app(0, nullptr);
-    main_app_crtp(0, nullptr);
+    // main_app_crtp(0, nullptr);
 
     return 0;
 }
--- main_triangle.cpp
+++ main_triangle.cpp
@@ -67,6 +67,83 @@ void create_triangle(unsigned int &vbo, unsigned int &vao, unsigned int &ebo)
 	glBindVertexArray(0);
 }
 
+// Variables for tracking dragging state
+static bool dragging = false;
+static ImVec2 drag_start(0.0f, 0.0f);
+static float translation[] = {0.0f, 0.0f};
+
+// Function to check if a point is inside the triangle
+bool is_point_in_triangle(float px, float py, float ax, float ay, float bx, float by, float cx, float cy)
+{
+    // Compute vectors and areas for the hit test
+    float v0x = cx - ax, v0y = cy - ay;
+    float v1x = bx - ax, v1y = by - ay;
+    float v2x = px - ax, v2y = py - ay;
+
+    float dot00 = v0x * v0x + v0y * v0y;
+    float dot01 = v0x * v1x + v0y * v1y;
+    float dot02 = v0x * v2x + v0y * v2y;
+    float dot11 = v1x * v1x + v1y * v1y;
+    float dot12 = v1x * v2x + v1y * v2y;
+
+    float denom = dot00 * dot11 - dot01 * dot01;
+    if (denom == 0.0f)
+        return false;
+
+    float inv_denom = 1.0f / denom;
+    float u = (dot11 * dot02 - dot01 * dot12) * inv_denom;
+    float v = (dot00 * dot12 - dot01 * dot02) * inv_denom;
+
+    return (u >= 0) && (v >= 0) && (u + v < 1);
+}
+
+void handle_drag(GLFWwindow *window, float& translation_x, float& translation_y)
+{
+    static bool dragging = false;
+    static float drag_start_x = 0.0f, drag_start_y = 0.0f;
+
+    // Get mouse button state and position
+    double mouse_x, mouse_y;
+    glfwGetCursorPos(window, &mouse_x, &mouse_y);
+
+    int window_width, window_height;
+    glfwGetFramebufferSize(window, &window_width, &window_height);
+
+    // Convert to OpenGL's normalized device coordinates
+    float mouse_ndc_x = (mouse_x / window_width) * 2.0f - 1.0f;
+    float mouse_ndc_y = 1.0f - (mouse_y / window_height) * 2.0f;
+
+    // Triangle vertices in NDC
+    float ax = translation_x + 0.0f, ay = translation_y + 0.25f;   // Top vertex
+    float bx = translation_x + 0.25f, by = translation_y - 0.25f;  // Bottom-right vertex
+    float cx = translation_x - 0.25f, cy = translation_y - 0.25f;  // Bottom-left vertex
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+    {
+        if (!dragging)
+        {
+            // Start dragging if the mouse is over the triangle
+            // if (is_point_in_triangle(mouse_ndc_x, mouse_ndc_y, ax, ay, bx, by, cx, cy))
+			if (true)
+            {
+                dragging = true;
+                drag_start_x = mouse_ndc_x - translation_x;
+                drag_start_y = mouse_ndc_y - translation_y;
+            }
+        }
+        else
+        {
+            // Update translation while dragging
+            translation_x = mouse_ndc_x - drag_start_x;
+            translation_y = mouse_ndc_y - drag_start_y;
+        }
+    }
+    else
+    {
+        dragging = false;
+    }
+}
+
 // renamed
 int main_triangle(int, char **)
 {
@@ -129,49 +206,51 @@ int main_triangle(int, char **)
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
 
-	while (!glfwWindowShouldClose(window))
+
+while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// feed inputs to dear imgui, start new frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+		handle_drag(window, translation[0], translation[1]);
 
-		// rendering our geometries
+		// Render triangle
 		triangle_shader.use();
+		triangle_shader.setUniform("translation", translation[0], translation[1]);
 		glBindVertexArray(vao);
 		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
-		// render your GUI
-		ImGui::Begin("Triangle Position/Color");
+		// Render ImGui
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Triangle Position");
+
+		// rotation
 		static float rotation = 0.0;
 		ImGui::SliderFloat("rotation", &rotation, 0, 2 * PI);
-		static float translation[] = {0.0, 0.0};
-		ImGui::SliderFloat2("position", translation, -1.0, 1.0);
-        static float color[4] = { 1.0f,1.0f,1.0f,1.0f };
         // pass the parameters to the shader
         triangle_shader.setUniform("rotation", rotation);
+
+		// translation is tracked outside of here
+		ImGui::SliderFloat2("Position", translation, -1.0f, 1.0f);
         triangle_shader.setUniform("translation", translation[0], translation[1]);
+
+		// color
+		static float color[4] = { 1.0f,1.0f,1.0f,1.0f };
         // color picker
         ImGui::ColorEdit3("color", color);
         // multiply triangle's color with this color
         triangle_shader.setUniform("color", color[0], color[1], color[2]);
-        ImGui::End();
 
-        ImGui::Begin("Conan logo");
-        render_conan_logo();
-        ImGui::End();
-		// Render dear imgui into screen
+		ImGui::End();
+
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
 		glfwSwapBuffers(window);
 	}
 
